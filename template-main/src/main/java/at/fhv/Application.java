9c1f1fe75dcec5f2b2e7f87fd32d2452f0e543b3
Fabian Falco <fazzer.crypot@hotmail.com>
2022-02-02 15:32:05 +0100

    add annotations

--- template-main/src/main/java/at/fhv/Application.java
+++ template-main/src/main/java/at/fhv/Application.java
@@ -2,10 +2,12 @@ package at.fhv;
 
 import javax.annotation.PostConstruct;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class Application {
 
   public static void main(String... args) {
