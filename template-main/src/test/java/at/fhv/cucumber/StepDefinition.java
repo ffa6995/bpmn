68cc9404f8a8b1ba27a8c5e5468eb263ef657eb9
Fabian Falco <fazzer.crypot@hotmail.com>
2022-02-04 21:26:53 +0100

    complete cucumber tests

--- template-main/src/test/java/at/fhv/cucumber/StepDefinition.java
+++ template-main/src/test/java/at/fhv/cucumber/StepDefinition.java
@@ -15,22 +15,62 @@ public class StepDefinition {
 
     @Given("Damage type is carScratch")
     public void damage_type_is_car_scratch() {
-        // Write code here that turns the phrase above into concrete actions
         String damageType = calculator.getDamageType();
-        assert (damageType.equals("carScratch"));
+        assertEquals(damageType, "carScratch");
     }
 
     @When("Damage Amount is {double}")
     public void damage_amount_is(Double amount) {
-        // Write code here that turns the phrase above into concrete actions
         Double damageAmount = calculator.getDamageAmount();
         assertEquals(damageAmount, amount);
     }
+
     @Then("Remuneration Amount is {double}")
     public void remuneration_amount_is(Double amount) {
-        // Write code here that turns the phrase above into concrete actions
         Double remunerationAmount = calculator.calculateRemuneration();
         assertEquals(remunerationAmount, amount);
+    }
+
+    CalculateRemuneration calculator2 = new CalculateRemuneration("totalDamage", 8000);
+    Double remuneration2 = calculator2.calculateRemuneration();
+
+    @Given("Damage type is totalDamage")
+    public void damage_type_is_total_damage() {
+        String damageType = calculator2.getDamageType();
+        assertEquals(damageType, "totalDamage");
+    }
+
+    @When("we have a damage Amount of {double}")
+    public void damage_amount_is2(Double amount) {
+        Double damageAmount = calculator2.getDamageAmount();
+        assertEquals(damageAmount, amount);
+    }
+
+    @Then("fixedRate is {double}")
+    public void fixed_rate_is(Double amount) {
+        Double fixedRate = calculator2.getFixedRate();
+        assertEquals(fixedRate, amount);
+    }
+
+    CalculateRemuneration calculator3 = new CalculateRemuneration("gearboxDamage", 0);
+    Double remuneration3 = calculator3.calculateRemuneration();
+
+    @Given("Damage amount is {double}")
+    public void damage_amount_is3(Double amount) {
+        Double damageAmount = calculator3.getDamageAmount();
+        assertEquals(damageAmount, damageAmount);
+    }
+
+    @When("there is damage type of gearboxDamage")
+    public void there_is_damage_type_of_gearbox_damage() {
+        String damageType = calculator3.getDamageType();
+        assertEquals(damageType, "gearboxDamage");
+    }
+
+    @Then("renumeration amount is {double}")
+    public void renumeration_amount_is2(Double zero) {
+        Double remunerationAmount = calculator3.getRemunerationAmount();
+        assertEquals(remunerationAmount, zero);
     }
 
 
--- template-main/src/test/resources/cucumber/TestProcess.feature
+++ template-main/src/test/resources/cucumber/TestProcess.feature
@@ -1,7 +1,17 @@
 Feature: Calculate Remuneration Amount
 
- Scenario: Calculate Remuneration Amount Based On Damage Type:
+ Scenario: Remuneration Amount gets calculated with car scratch damage:
    Given Damage type is carScratch
    When Damage Amount is 500
    Then Remuneration Amount is 475
+
+  Scenario: The fixedRate on a totalDamage should be correct:
+    Given Damage type is totalDamage
+    When we have a damage Amount of 8000
+    Then fixedRate is 0.80
+
+  Scenario: There is no input for damage amount given:
+    Given Damage amount is 0
+    When there is damage type of gearboxDamage
+    Then renumeration amount is 0
 
