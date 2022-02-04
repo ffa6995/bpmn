5d292a3ede8823b6d49f320558f418edc3d98ce7
YaPeKaKra <yannick.krampl@hotmail.com>
2022-02-04 18:41:29 +0100

    commit

--- template-main/src/test/java/at/fhv/cucumber/CucumberTest.java
+++ template-main/src/test/java/at/fhv/cucumber/CucumberTest.java
@@ -4,8 +4,15 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/")
+@CucumberOptions(
+        features = "src/test/resources/TestProcess.feature",
+        glue = "MyStepdefs")
+
 public class CucumberTest {
+
+
 }
+
 
--- template-main/src/test/resources/TestProcess.feature
+++ template-main/src/test/resources/TestProcess.feature
@@ -1,6 +1,7 @@
-Feature:
+Feature: Calculate Remuneration Amount
 
- Scenario:
-   Given
-   When
-   Then
+ Scenario: Calculate Remuneration Amount Based On Damage Type:
+   Given Damage type is car scratch
+   When Damage Amount is 500
+   Then Remuneration Amount is 475
+
