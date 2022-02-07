e325e51e48e1b2a772fa5a035c6a5a2db3d2eb11
Fabian Falco <fazzer.crypot@hotmail.com>
2022-02-07 18:43:44 +0100

    add form to send confirmation

--- src/test/java/config/Config.java
+++ src/test/java/config/Config.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages ="at.fhv")
-class TestConfiguration {
+class Config {
 // defines a webdriver Bean
 }
